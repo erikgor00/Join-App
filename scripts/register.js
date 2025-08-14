f7553b244999a36ce9d801bae7410839c96e265b
Erik Gorzawski <erik-gorzawski@hotmail.com>
2025-08-14 23:35:19 +0200

    Added SignUp JS; Push to Users

--- scripts/register.js
+++ scripts/register.js
@@ -1,7 +1,13 @@
 function addUser() {
-    let email = document.getElementById('email');
-    let password = document.getElementById('password');
-    users.push({email: email.value, password: password.value});
-    // Weiterleitung zur Login Seite zum Beispiel + Nachricht anzeigen, dass registrierung erfolgreich war
-    window.location.href = 'login.html?msg=Du hast dich erfolgrteich registriert!'
+    let email = document.getElementById('registerEmail');
+    let password = document.getElementById('registerPassword');
+    let confirmPassword = document.getElementById('registerPasswordConfirm');
+    if (password.value === confirmPassword.value) {
+         users.push({email: email.value, password: password.value});
+        // Weiterleitung zur Login Seite zum Beispiel + Nachricht anzeigen, dass registrierung erfolgreich war
+        window.location.href = 'login.html?msg=Du hast dich erfolgrteich registriert!'
+    } else {
+        alert("Die eingegebnen Passwörter stimmen nicht überein!")
+    }
 }
+
